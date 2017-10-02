328c0f4a5eaad051ecb1a62604cfcc89a5a824fa
Oladele Femi <profemzy@gmail.com>
2017-10-02 23:34:18 +0100

    Completed User model with create blog and post functionality

--- src/models/user.py
+++ src/models/user.py
@@ -1,3 +1,4 @@
+import datetime
 import uuid
 
 from flask import session
@@ -53,6 +54,16 @@ class User(object):
 
     def get_blogs(self):
         return Blog.find_by_author_id(self._id)
+
+    def new_blog(self, title, description):
+        blog = Blog(author=self.name, title=title, description=description,
+                    author_id=self._id)
+        blog.save_to_mongo()
+
+    @staticmethod
+    def new_post(blog_id, title, content, date=datetime.datetime.utcnow()):
+        blog = Blog.from_mongo(blog_id)
+        blog.new_post(title=title, content=content, date=date)
 
     def json(self):
         return {
